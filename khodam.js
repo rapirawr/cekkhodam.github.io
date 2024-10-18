8e5ba63767a17d2e4207d7d7862c604f898dc955
PaisSigma <adhyastapratama2312@gmail.com>
2024-10-18 12:37:53 +0700

    khodam.js

--- khodam.js
+++ khodam.js
@@ -13,7 +13,9 @@ Sugikocument.getElementById('checkButton').addEventListener('click', function() 
   function checkKodam(name) {
       const khodams = [ 
             "Burung Emas 🦅💛", "Guntur Biru 🔵", "Hantu Bayangan 🌑", "Bintang Emas 🌟", "Angin Dingin ❄️","Agus Tiger",
-            "Uyung Berkaki lima", "Eko berkepala naga", "Dajjal pujer", "Barong", "Imron kentut api" , "Sugik anjay", " Uyung Kopling", "Sugik Togel", "Slamet Michat", "Bakso Uyung", "Basori Pecel Lele", "Siluman Nanang", "Agus Jembatan Ancol", "Eko Moshing", " Uyung Breakdance", "Sugik Molet", "Nono Gombel", "Agus Mohawk", "Bashori Overthinking", "Sugik Introvert"
+            "Uyung Berkaki lima", "Eko berkepala naga", "Dajjal pujer", "Barong", "Imron kentut api" , "Sugik anjay", " Uyung Kopling",
+            "Sugik Togel", "Slamet Michat", "Bakso Uyung", "Basori Pecel Lele", "Siluman Nanang", "Agus Jembatan Ancol", "Eko Moshing",
+            " Uyung Breakdance", "Sugik Molet", "Nono Gombel", "Agus Mohawk", "Bashori Overthinking", "Sugik Introvert"
         ];
           
       const index = Math.floor(Math.random() * khodams.length);
